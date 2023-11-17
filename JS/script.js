821369f1b7a28112d334e9adbba36f5427552a92
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-17 15:12:37 +0100

    applicazione sconti

--- JS/script.js
+++ JS/script.js
@@ -17,15 +17,33 @@ const etaPassegero = parseInt(prompt("Qaunti anni hai?"));
 console.log("età passegero:", etaPassegero);
 
 // calcolo costo biglietto senza sconti
-const costoBigliettoIniziale = kmViaggio * 0.21;
-console.log("costo del biglietto senza sconti:", costoBigliettoIniziale, "€");
+const costoBigliettoIniziale = parseInt(kmViaggio * 0.21);
+console.log("costo del biglietto senza sconti:", costoBigliettoIniziale,"€");
 
 // calcolo sconto 20%
-const scontoVenti = costoBigliettoIniziale * 20 / 100;
-console.log("questo è lo sconto del 20%:",scontoVenti);
+const scontoVenti = parseInt((costoBigliettoIniziale * 20 / 100).toFixed(2));
+console.log("questo è lo sconto del 20%:",scontoVenti,"€");
 
 // calcolo sconto 40%
-const scontoQuaranta = costoBigliettoIniziale * 40 / 100;
-console.log("questo è lo sconto del 40%:", scontoQuaranta);
+const scontoQuaranta = parseInt((costoBigliettoIniziale * 40 / 100).toFixed(2));
+console.log("questo è lo sconto del 40%:", scontoQuaranta,"€");
 
 // applicazione sconti
+if (etaPassegero < 18) {
+    let costoBigliettoFinale = parseInt((costoBigliettoIniziale - scontoVenti.toFixed(2)));
+    console.log("questo è il prezzo finale del biglietto:",costoBigliettoFinale,"€");
+} else if (etaPassegero > 65) {
+    let costoBigliettoFinale = parseInt((costoBigliettoIniziale - scontoQuaranta.toFixed(2)));
+    console.log("questo è il prezzo finale del biglietto:",costoBigliettoFinale,"€");
+} else {
+    costoBigliettoFinale = parseInt(costoBigliettoIniziale);
+    console.log("questo è il prezzo finale del biglietto:",costoBigliettoFinale,"€");
+}
+
+
+// km 100
+// under 18 16.8
+// over 12.6
+// normale 21
+
+// 317 
